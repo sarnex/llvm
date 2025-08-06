8eaf15f965e95fb1aac110d9b9d8b63f3370e41c
Sarnie, Nick <nick.sarnie@intel.com>
2025-08-06 13:16:42 -0700

    test
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- unified-runtime/cmake/FetchLevelZero.cmake
+++ unified-runtime/cmake/FetchLevelZero.cmake
@@ -45,7 +45,7 @@ endif()
 #     message(STATUS "Level Zero Adapter: Copying Level Zero headers to local build tree")
 #     file(COPY ${UR_LEVEL_ZERO_INCLUDE_DIR}/ DESTINATION ${LEVEL_ZERO_COPY_DIR})
 # endif()
-if(UR_COMPUTE_RUNTIME_FETCH_REPO OR NOT LEVEL_ZERO_LIB_NAME OR NOT LEVEL_ZERO_INCLUDE_DIR)
+if(UR_COMPUTE_RUNTIME_FETCH_REPO OR NOT LEVEL_ZERO_LIB_NAME AND NOT LEVEL_ZERO_LIBRARY)
 
     message(STATUS "Level Zero Adapter: Download Level Zero loader and headers from github.com")
 
